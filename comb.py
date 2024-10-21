fb5d7a6fdb67e855d626c6e1a0ea1eafbb394d57
senadawr <153177664+senadawr@users.noreply.github.com>
2024-10-21 23:37:39 +0800

    Update comb.py

--- comb.py
+++ comb.py
@@ -1,24 +1,22 @@
-def getNextGap(gap):
-    gap = (gap * 10)//13
-    if gap < 1:
-        return 1
-    return gap
-
 def combSort(a):
     n = len(a)
     gap = n
-
     swapped = True
 
-    while gap != 1 or swapped == True:
-        gap = getNextGap(gap)
+    while gap > 1 or swapped:
+        gap = int(gap//1.3)
+
+        if gap < 1:
+            gap = 1
+        
         swapped = False
 
-        for i in range(0, n-gap):
+        for i in range(n - gap):
+
             if a[i] > a[i+gap]:
                 a[i],a[i+gap] = a[i+gap],a[i]
                 swapped = True
-
+        
 a = [9, 3, 2, 5]
 print(a)
 combSort(a)
