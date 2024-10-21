bf276fab3f9143e1c8d785f517e0775f7ff60284
senadawr <153177664+senadawr@users.noreply.github.com>
2024-10-21 23:37:17 +0800

    Update shell.py

--- shell.py
+++ shell.py
@@ -1,24 +1,20 @@
-def shellSort(a, n):
+def shellSort(a):
+    n = len(a)
     gap = n//2
 
     while gap > 0:
-        j = gap
+        for i in range(gap, n):
+            temp = a[i]
+            j = i
+            while j >= gap and a[j - gap] > temp:
+                a[j] = a[j - gap]
+                j -= gap
+        
+            a[j] = temp
 
-        while j < n:
-            i = j - gap
-
-            while i >= 0:
-                if a[i + gap] > a[i]:
-                    break
-                else:
-                    a[i + gap], a[i] = a[i], a[i + gap]
-                i = i-gap
-
-            j += 1
-        gap = gap//2
-
+        gap //= 2
+        
 a = [9, 3, 2, 5]
-n = len(a)
 print(a)
-shellSort(a, n)
+shellSort(a)
 print(a)
